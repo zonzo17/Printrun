67267539d0d50817d73dd802147b8f3857d32653
zonzo17 <cristhian.hoyos@hotmail.com>
2019-10-12 16:06:30 -0400

    Update print_test.py

--- print_test.py
+++ print_test.py
@@ -6,7 +6,7 @@ atexit.register(lambda: input("\nPress Enter to exit."))
 import time
 from printrun.printcore import printcore
 
-p = printcore('/dev/ttyACM0', 115200)
+p = printcore('COM4', 115200)
 
 # Wait for connection to the printer
 print('Waiting for printer ...')
@@ -17,6 +17,7 @@ print('Connected to printer !')
 
 print('Sending home instruction (G28)')
 p.send('G28')
+p.send('G92 X10 Y100 Z50')
 
 print('Disconnecting')
 p.disconnect()
