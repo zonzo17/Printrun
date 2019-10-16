d64bc41aaac6582d0197ef74d253e875e34e35aa
zonzo17 <cristhian.hoyos@hotmail.com>
2019-10-16 13:40:04 -0400

    Ajout arduino

--- print_test.py
+++ print_test.py
@@ -3,21 +3,31 @@
 import atexit
 atexit.register(lambda: input("\nPress Enter to exit."))
 
+print('          YOOOOO WHATS UP CRONUS !!!           ')
+
+#import serial
 import time
 from printrun.printcore import printcore
 
-p = printcore('COM4', 115200)
+p = printcore('COM5', 115200)
+#ArduinoUnoSerial = serial.Serial('COM25',9600)
+#time.sleep(2)
+#while 1:
+ #   arduinoData = ArduinoUnoSerial.readline()
+    
 
 # Wait for connection to the printer
 print('Waiting for printer ...')
 
 while not p.online:
-    time.sleep(20)
+    time.sleep(5)
 print('Connected to printer !')
 
 print('Sending home instruction (G28)')
-p.send('G28')
-p.send('G92 X10 Y100 Z50')
+#p.send('G28')
+print('Sending X Y Z instructions (G92)')
+
+p.send('G1 X200 Y200 Z200')
 
 print('Disconnecting')
 p.disconnect()
